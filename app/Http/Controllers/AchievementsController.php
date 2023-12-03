634489d453256522267905a9d4bff75f8a7666ad
Joaz Rivera <joazrivera@gmail.com>
2023-12-03 00:23:05 -0600

    feat: Update AchievementsController to return dynamic user achievements data

--- app/Http/Controllers/AchievementsController.php
+++ app/Http/Controllers/AchievementsController.php
@@ -7,14 +7,82 @@ use Illuminate\Http\Request;
 
 class AchievementsController extends Controller
 {
+    /**
+     * Display a listing of the user's achievements and badges.
+     *
+     * @param  User  $user
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function index(User $user)
     {
+        // Retrieve user's unlocked achievements
+        $unlockedAchievements = $user->achievements->pluck('name');
+
+        // Determine the next available achievements
+        $nextAvailableAchievements = $this->getNextAvailableAchievements($user);
+
+        // Determine current and next badge
+        $currentBadge = $this->getCurrentBadge($user);
+        $nextBadge = $this->getNextBadge($user);
+
+        // Calculate remaining achievements to unlock next badge
+        $remainingToUnlockNextBadge = $this->getRemainingToUnlockNextBadge($user, $nextBadge);
+
         return response()->json([
-            'unlocked_achievements' => [],
-            'next_available_achievements' => [],
-            'current_badge' => '',
-            'next_badge' => '',
-            'remaing_to_unlock_next_badge' => 0
+            'unlocked_achievements' => $unlockedAchievements,
+            'next_available_achievements' => $nextAvailableAchievements,
+            'current_badge' => $currentBadge,
+            'next_badge' => $nextBadge,
+            'remaining_to_unlock_next_badge' => $remainingToUnlockNextBadge
         ]);
+    }
+
+    /**
+     * Get the next available achievements for the user.
+     *
+     * @param  User  $user
+     * @return array
+     */
+    private function getNextAvailableAchievements(User $user)
+    {
+        // TODO: Seed achievements with rules
+        return ['5 Lessons Watched'];
+    }
+
+    /**
+     * Get the current badge of the user.
+     *
+     * @param  User  $user
+     * @return string
+     */
+    private function getCurrentBadge(User $user)
+    {
+        // TODO: Calculate current user's badge
+        return '';
+    }
+
+    /**
+     * Get the next badge the user can earn.
+     *
+     * @param  User  $user
+     * @return string
+     */
+    private function getNextBadge(User $user)
+    {
+        // TODO: Calculate current user's next badge
+        return '';
+    }
+
+    /**
+     * Calculate the number of achievements the user needs to unlock the next badge.
+     *
+     * @param  User  $user
+     * @param  string  $nextBadge
+     * @return int
+     */
+    private function getRemainingToUnlockNextBadge(User $user, $nextBadge)
+    {
+        // TODO: Implement the logic to calculate remaining achievements for next badge
+        return 0;
     }
 }
