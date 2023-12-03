9376607bfb1d83ae40b2e92636947db54af86e0c
Joaz Rivera <joazrivera@gmail.com>
2023-12-03 01:17:35 -0600

    feat: Implement unlocking of '5 Lessons Watched' achievement

--- tests/Feature/UserAchievementsTest.php
+++ tests/Feature/UserAchievementsTest.php
@@ -43,4 +43,32 @@ class UserAchievementsTest extends TestCase
                      'remaining_to_unlock_next_badge' => 0
                 ]);
     }
+
+    /**
+     * Test to ensure a user unlocks the '5 Lessons Watched' achievement 
+     * after watching their 5 lessons.
+     */
+    public function testUserUnlocksFiveLessonsWatchedAchievement()
+    {
+        $user = User::factory()->create();
+        $lessons = Lesson::factory()->count(5)->create();
+
+        foreach ($lessons as $lesson) {
+            // Update the pivot table to reflect that the lesson is watched
+            // I made this way due the challenge rule to avoid any logic on the LessonWatched/CommentWritten events. 
+            $user->watched()->attach($lesson, ['watched' => true]);
+            $user->refresh();
+            
+            // Dispatch the LessonWatched event
+            event(new LessonWatched($lesson, $user));
+        }
+
+        $response = $this->get("/users/{$user->id}/achievements");
+        $response->assertStatus(200)
+                ->assertJson([
+                    'unlocked_achievements' => ['First Lesson Watched', '5 Lessons Watched'],
+                    // ... other expected fields
+                ]);
+    }
+
 }
