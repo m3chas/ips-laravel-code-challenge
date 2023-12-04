016c6edd22f0ee991c1c865d2802028adf1b4cd0
Joaz Rivera <joazrivera@gmail.com>
2023-12-04 14:30:38 -0600

    refactor: Optimize achievement tests to support multiple achievements

--- tests/Feature/UserAchievementsTest.php
+++ tests/Feature/UserAchievementsTest.php
@@ -40,13 +40,13 @@ class UserAchievementsTest extends TestCase
         // Make a GET request to the achievements endpoint and assert the expected response
         $response = $this->get("/users/{$user->id}/achievements");
         $response->assertStatus(200)
-                 ->assertJson([
-                     'unlocked_achievements' => ['First Lesson Watched'],
-                     'next_available_achievements' => ['5 Lessons Watched'],
-                     'current_badge' => '',
-                     'next_badge' => '',
-                     'remaining_to_unlock_next_badge' => 0
-                ]);
+            ->assertJson([
+                'unlocked_achievements' => ['First Lesson Watched'],
+                'next_available_achievements' => ['5 Lessons Watched'],
+                'current_badge' => '',
+                'next_badge' => '',
+                'remaining_to_unlock_next_badge' => 0
+            ]);
     }
 
     /**
@@ -73,7 +73,6 @@ class UserAchievementsTest extends TestCase
 
         // Make a GET request to the achievements endpoint and assert the expected response
         $response = $this->get("/users/{$user->id}/achievements");
-
         $response->assertStatus(200)
             ->assertJson([
                 'unlocked_achievements' => ['First Lesson Watched', '5 Lessons Watched'],
@@ -81,7 +80,103 @@ class UserAchievementsTest extends TestCase
                 'current_badge' => '',
                 'next_badge' => '',
                 'remaining_to_unlock_next_badge' => 0
-        ]);
+            ]);
+    }
+
+    public function testUserUnlocksTenLessonsWatchedAchievement()
+    {
+        // Generate necesary data for this test.
+        Achievement::factory()->create(['name' => 'First Lesson Watched', 'required_count' => 1]);
+        Achievement::factory()->create(['name' => '5 Lessons Watched', 'required_count' => 5]);
+        Achievement::factory()->create(['name' => '10 Lessons Watched', 'required_count' => 10]);
+        $user = User::factory()->create();  
+        $lessons = Lesson::factory()->count(10)->create();
+
+        foreach ($lessons as $lesson) {
+            // Update the pivot table to reflect that the lesson is watched
+            // I made this way due the challenge rule to avoid any logic on the LessonWatched/CommentWritten events. 
+            $user->watched()->attach($lesson, ['watched' => true]);
+            $user->refresh();
+            
+            // Dispatch the LessonWatched event
+            event(new LessonWatched($lesson, $user));
+        }
+
+        // Make a GET request to the achievements endpoint and assert the expected response
+        $response = $this->get("/users/{$user->id}/achievements");
+        $response->assertStatus(200)
+            ->assertJson([
+                'unlocked_achievements' => ['First Lesson Watched', '5 Lessons Watched', '10 Lessons Watched'],
+                'next_available_achievements' => ['5 Lessons Watched'],
+                'current_badge' => '',
+                'next_badge' => '',
+                'remaining_to_unlock_next_badge' => 0
+            ]);
+    }
+
+    public function testUserUnlocksTwentyFiveLessonsWatchedAchievement()
+    {
+        // Generate necesary data for this test.
+        Achievement::factory()->create(['name' => 'First Lesson Watched', 'required_count' => 1]);
+        Achievement::factory()->create(['name' => '5 Lessons Watched', 'required_count' => 5]);
+        Achievement::factory()->create(['name' => '10 Lessons Watched', 'required_count' => 10]);
+        Achievement::factory()->create(['name' => '25 Lessons Watched', 'required_count' => 25]);
+        $user = User::factory()->create();  
+        $lessons = Lesson::factory()->count(25)->create();
+
+        foreach ($lessons as $lesson) {
+            // Update the pivot table to reflect that the lesson is watched
+            // I made this way due the challenge rule to avoid any logic on the LessonWatched/CommentWritten events. 
+            $user->watched()->attach($lesson, ['watched' => true]);
+            $user->refresh();
+            
+            // Dispatch the LessonWatched event
+            event(new LessonWatched($lesson, $user));
+        }
+
+        // Make a GET request to the achievements endpoint and assert the expected response
+        $response = $this->get("/users/{$user->id}/achievements");
+        $response->assertStatus(200)
+            ->assertJson([
+                'unlocked_achievements' => ['First Lesson Watched', '5 Lessons Watched', '10 Lessons Watched', '25 Lessons Watched'],
+                'next_available_achievements' => ['5 Lessons Watched'],
+                'current_badge' => '',
+                'next_badge' => '',
+                'remaining_to_unlock_next_badge' => 0
+            ]);
+    }
+
+    public function testUserUnlocksFiftyLessonsWatchedAchievement()
+    {
+        // Generate necesary data for this test.
+        Achievement::factory()->create(['name' => 'First Lesson Watched', 'required_count' => 1]);
+        Achievement::factory()->create(['name' => '5 Lessons Watched', 'required_count' => 5]);
+        Achievement::factory()->create(['name' => '10 Lessons Watched', 'required_count' => 10]);
+        Achievement::factory()->create(['name' => '25 Lessons Watched', 'required_count' => 25]);
+        Achievement::factory()->create(['name' => '50 Lessons Watched', 'required_count' => 50]);
+        $user = User::factory()->create();  
+        $lessons = Lesson::factory()->count(50)->create();
+
+        foreach ($lessons as $lesson) {
+            // Update the pivot table to reflect that the lesson is watched
+            // I made this way due the challenge rule to avoid any logic on the LessonWatched/CommentWritten events. 
+            $user->watched()->attach($lesson, ['watched' => true]);
+            $user->refresh();
+            
+            // Dispatch the LessonWatched event
+            event(new LessonWatched($lesson, $user));
+        }
+
+        // Make a GET request to the achievements endpoint and assert the expected response
+        $response = $this->get("/users/{$user->id}/achievements");
+        $response->assertStatus(200)
+            ->assertJson([
+                'unlocked_achievements' => ['First Lesson Watched', '5 Lessons Watched', '10 Lessons Watched', '25 Lessons Watched', '50 Lessons Watched'],
+                'next_available_achievements' => ['5 Lessons Watched'],
+                'current_badge' => '',
+                'next_badge' => '',
+                'remaining_to_unlock_next_badge' => 0
+            ]);
     }
 
 }
